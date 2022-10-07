29f032871e06801bb8caf070bd5aeecbe6b44610
Brandon Lamer-Connolly <brandon.lc.1334@gmail.com>
2022-10-07 17:20:08 -0400

    more work on the grid

--- TTTGUI/src/Grid.java
+++ TTTGUI/src/Grid.java
@@ -1,6 +1,16 @@
 import javax.swing.*;
 import java.awt.*;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Grid extends JPanel {
+
+    private ArrayList<GridSpace> grid;
+
+    public Grid() {
+        grid = new ArrayList<GridSpace>();
+        for (int i = 0; i < 9; i++) {
+            grid.add(new GridSpace(i));
+        }
+    }
 }
--- TTTGUI/src/GridSpace.java
+++ TTTGUI/src/GridSpace.java
@@ -1,6 +1,22 @@
 import javax.swing.*;
-import java.awt.*;
-import java.util.ArrayList;
+enum State {X, O, EMPTY};
 
 public class GridSpace extends JButton {
+    private int indentifer;
+    private State currentState;
+    public GridSpace(int indentifer) {
+        this.indentifer = indentifer;
+        this.currentState = State.EMPTY;
+    }
+
+    public GridSpace(int indentifer, State state) {
+        this.indentifer = indentifer;
+        this.currentState = state;
+    }
+    public int getIndentifer() {
+        return this.indentifer;
+    }
+    public void setCurrentState(State state) {
+        this.currentState = state;
+    }
 }
