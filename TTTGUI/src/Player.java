3f11a0be6138238af88bb594fa6859f4fe50775f
Brandon Lamer-Connolly <brandon.lc.1334@gmail.com>
2022-10-07 16:58:42 -0400

    starting work

---
+++ TTTGUI/src/Player.java
@@ -0,0 +1,11 @@
+public class Player {
+    private String name;
+    private boolean isX;
+    private boolean starts;
+
+    public Player(String name, boolean isX, boolean starts) {
+        this.name = name;
+        this.isX = isX;
+        this.starts = starts;
+    }
+}
---
+++ TTTGUI/src/TicTacToe.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class TicTacToe {
+    public static boolean player1Turn;
+
+
+}
